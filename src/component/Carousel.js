c0c68e74c59e10ab3e712dc1b0cf0939c3475110
rdelena <rodnydelena@gmail.com>
2023-08-04 14:24:02 -0500

    carousel img added

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -1,19 +1,50 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import Image1 from "../img/gaoryn_screenshot1.jpg";
-import Image2 from "../img/gaoryn_screenshot2.jpg";
+import Image1 from "../img/NPCDialog.jpg";
+import Image2 from "../img/ForestFight.jpg";
+import Image3 from "../img/Town2.jpg";
+import Image4 from "../img/Temple.jpg";
+import Image5 from "../img/Homestead.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../CarouselContainer.css";
 
 const ImageCarousel = () => {
   return (
-    <Carousel style={{ height: "400px", width: "400px" }}>
-      <div>
+    <Carousel showStatus={false} showThumbs={false} autoPlay infiniteLoop>
+      <div className="slide">
         <img src={Image1} alt="MapScreenshot1" />
-        <p>Map Overview of the Homestead & Twilight's Anchor</p>
+        <div className="slide-content">
+          <h3>Slide 1 Title</h3>
+          <p>Slide 1 Description</p>
+        </div>
       </div>
-      <div>
+      <div className="slide">
         <img src={Image2} alt="MapScroonshot2" />
-        <p>Ainzgale Forest</p>
+        <div className="slide-content">
+          <h3>Slide 2 Title</h3>
+          <p>Slide 2 Description</p>
+        </div>
+      </div>
+      <div className="slide">
+        <img src={Image3} alt="MapScroonshot2" />
+        <div className="slide-content">
+          <h3>Slide 3 Title</h3>
+          <p>Slide 3 Description</p>
+        </div>
+      </div>
+      <div className="slide">
+        <img src={Image4} alt="MapScroonshot2" />
+        <div className="slide-content">
+          <h3>Slide 4 Title</h3>
+          <p>Slide 4 Description</p>
+        </div>
+      </div>
+      <div className="slide">
+        <img src={Image5} alt="MapScroonshot2" />
+        <div className="slide-content">
+          <h3>Slide 5 Title</h3>
+          <p>Slide 5 Description</p>
+        </div>
       </div>
     </Carousel>
   );
--- src/component/Landing.js
+++ src/component/Landing.js
@@ -4,6 +4,7 @@ import ImageCarousel from "./Carousel";
 const Landing = () => {
   return (
     <div>
+      <h1>Journey into the Land of Gaoryn</h1>
       <ImageCarousel />
     </div>
   );
